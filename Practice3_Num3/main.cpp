614e65e11f63ef3ed7126ab0e8f9f0c2c7bdbd51
ResDog777 <88165787+ResDog777@users.noreply.github.com>
2023-04-20 18:30:42 +0300

    Add files via upload

---
+++ Practice3_Num3/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "bay.h"
+#include "ocean.h"
+#include "sea.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Ocean ocean;
+	cin >> ocean;
+	cout << ocean;
+
+	Bay bay;
+	cin >> bay;
+	cout << bay;
+	bay.add_parent(ocean);
+
+	Sea sea;
+	cin >> sea;
+	cout << sea;
+	sea.add_parent(ocean);
+	sea.add_child(bay);
+
+	return 0;
+}
