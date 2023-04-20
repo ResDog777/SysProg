c449a7ea231121a6637de044318f0a8d3d3fa3c7
ResDog777 <88165787+ResDog777@users.noreply.github.com>
2023-04-20 19:02:35 +0300

    Add files via upload

---
+++ Practice4_Num1/My_class.cpp
@@ -0,0 +1,22 @@
+#include "My_class.h"
+#include <iostream>
+
+using namespace std;
+
+My_class::My_class(int a, float b)
+{
+	this->a = a;
+	this->b = b;
+}
+
+void My_class::Out()
+{
+	cout << a << " " << b << endl;
+}
+
+My_class My_class::operator = (My_class& T)
+{
+	a = T.a;
+	b = T.b;
+	return *this;
+}
---
+++ Practice4_Num1/main.cpp
@@ -0,0 +1,33 @@
+﻿#include "My_class.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x = 1;
+	int y = 2;
+	cout << "Перед обменом" << endl;
+	OutXY(x, y);
+	Swap(x, y);
+	cout << "После обмена" << endl;
+	OutXY(x, y);
+
+	float x1 = 1.5;
+	float y1 = 5.9;
+	cout << "Перед обменом" << endl;
+	OutXY(x1, y1);
+	Swap(x1, y1);
+	cout << "После обмена" << endl;
+	OutXY(x1, y1);
+
+	My_class t1(1, 1.5), t2(2, 9.9);
+	cout << "Перед обменом" << endl;
+	t1.Out();
+	t2.Out();
+	Swap(t1, t2);
+	cout << "После обмена" << endl;
+	t1.Out();
+	t2.Out();
+}
